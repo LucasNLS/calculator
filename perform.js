2007ecbce08ccca5ebd2128b0894066ba9751828
Lucas <62632191+LucasNLS@users.noreply.github.com>
2023-01-05 20:48:15 -0600

    0 isn't displayed when pressed first

--- perform.js
+++ perform.js
@@ -36,24 +36,57 @@ function operator(num1, operator, num2) {
 
 function logText(e) {
   const classes = this.classList.value.split(" ");
-  if (classes[1] === "equal") {
+  if (classes[1] === "clear") {
     input.textContent = "";
-    //Calculate what is on div
+    while (calculate.length > 0) {
+      calculate.pop();
+    }
+    console.log("clear");
   } else {
     if (classes[2] === "number") {
       switch (classes[1]) {
         case "one":
           calculate.push(1);
           input.textContent += "1";
+          break;
         case "two":
-          calculate.push(1);
-          input.textContent += "1";
+          calculate.push(2);
+          input.textContent += "2";
+          break;
         case "three":
-          calculate.push(1);
-          input.textContent += "1";
+          calculate.push(3);
+          input.textContent += "3";
+          break;
         case "four":
-          calculate.push(1);
-          input.textContent += "1";
+          calculate.push(4);
+          input.textContent += "4";
+          break;
+        case "five":
+          calculate.push(5);
+          input.textContent += "5";
+          break;
+        case "six":
+          calculate.push(6);
+          input.textContent += "6";
+          break;
+        case "seven":
+          calculate.push(7);
+          input.textContent += "7";
+          break;
+        case "eight":
+          calculate.push(8);
+          input.textContent += "8";
+          break;
+        case "nine":
+          calculate.push(9);
+          input.textContent += "9";
+          break;
+        case "zero":
+          if (calculate[0] != undefined) {
+            calculate.push(0);
+            input.textContent += "0";
+          }
+          break;
       }
     }
     //  switch (classes[1]){
@@ -62,7 +95,6 @@ function logText(e) {
     //}
     //input.textContent += `${classes[1]} `;
   }
-  console.log(calculate);
 }
 
 let calculate = [];
